6108fea682673b614b6532e6915ccea9b3c83852
sergunya95 <sergunya95@gmail.com>
2020-10-13 23:58:02 +0700

    Initial Commit

---
+++ TwoBlocksHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoBlocksHomeWork
+//
+//  Created by apple on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
